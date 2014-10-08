49778cfeafe4d51d2ae717da4a9502c95ed7d7d2
studentzzb <studentzzb@163.com>
2014-10-08 17:08:49 +0800

    [code]another main test

---
+++ api-spi/src/main/java/com/dhgate/open/spi/AnotherMainTest.java
@@ -0,0 +1,12 @@
+package com.dhgate.open.spi;
+
+public class AnotherMainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("this is another spi test");
+	}
+
+}
