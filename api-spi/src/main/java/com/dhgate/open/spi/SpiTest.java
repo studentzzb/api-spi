2b09853ffcbbf7b4d09276ee6cd383a1a45537bb
studentzzb <studentzzb@163.com>
2014-10-08 16:50:11 +0800

    [code]the first git project api-spi init.

---
+++ api-spi/src/main/java/com/dhgate/open/spi/SpiTest.java
@@ -0,0 +1,12 @@
+package com.dhgate.open.spi;
+
+public class SpiTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("this is a git project spi");
+	}
+
+}
